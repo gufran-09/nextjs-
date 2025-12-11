0aa095a4180c70746780d2bf22e7f14989ed62bf
Gufran Ahmed <gufranahmed0921@gmail.com>
2025-12-11 16:28:16 +0530

    Implemented Eslint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,47 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import standard from "eslint-config-standard";
+import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+// Import the plugin
+import importPlugin from "eslint-plugin-import";
+
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  standard,
 
-export default eslintConfig;
+  // Add eslint-plugin-import
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // Recommended useful import rules
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/no-duplicates": "warn",
+    },
+  },
+
+  // Add Prettier last (to disable conflicting rules)
+  prettier,
+
+  // Global ignores
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
